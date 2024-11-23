3d319068e20f5fb3a44049014e48809466d5a354
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2024-11-23 16:32:53 +0100

    added book stats

--- books_API/src/App.tsx
+++ books_API/src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Books } from './Books';
+import { BookStats } from './BookStats';
 
 const queryClient = new QueryClient();
 
 const App = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
+			<BookStats />
 			<Books />
 		</QueryClientProvider>
 	);
--- books_API/src/BookStats.tsx
+++ books_API/src/BookStats.tsx
@@ -1,1 +1,13 @@
-export const BookStats = () => {};
+import { useGetBooksQuery } from './queries/useGetBooksQuery';
+
+export const BookStats = () => {
+	const { data, isFetching } = useGetBooksQuery();
+
+	if (isFetching) return <p>Loading book stats...</p>;
+
+	return (
+		<div>
+			Books count: <strong>{data?.length || 'No data'}</strong>
+		</div>
+	);
+};
