af7f260198c032c609bf63539157a9249563938f
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2024-11-26 15:55:15 +0100

    added styles to Button and Text

--- styles-exercises/src/App.tsx
+++ styles-exercises/src/App.tsx
@@ -1,5 +1,32 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { Button } from './Button';
+import { Text } from './Text';
+import { Wrapper } from './Wrapper';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${props => props.theme.colors.background};
+  }
+`;
+
 const App = () => {
-	return null;
+	const light = {
+		colors: {
+			primary: '#0333EE',
+			textPrimary: '#fff',
+			background: '#eee',
+			textBackground: '#333',
+		},
+	};
+	return (
+		<ThemeProvider theme={light}>
+			<GlobalStyle />
+			<Wrapper>
+				<Button label='Lorem ipsum' />
+				<Text>Dolor sit amet</Text>
+			</Wrapper>
+		</ThemeProvider>
+	);
 };
 
 export { App };
---
+++ styles-exercises/src/Text.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+type TextProps = {
+	children: ReactNode;
+};
+
+const StyledText = styled.p`
+	color: ${props => props.theme.colors.textBackground};
+`;
+
+export const Text = ({ children }: TextProps) => {
+	return <StyledText>{children}</StyledText>;
+};
